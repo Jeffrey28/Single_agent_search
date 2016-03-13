0652751584ae7bb15d234f191bb9daef916ad02f
changliuucb <shionlau@gmail.com>
2016-03-12 16:18:33 -0800

    split multi_agent_search and single_agent_search subfolders and created two separate repos instead.

--- multi_agent_search/A_fct2s.m
+++
@@ -1,6 +0,0 @@
-function value = A_fct2s(lambda,psi)
-% a short version (this is why there's a 's' in the function name)
-% of the log partition function in exponential family for Gaussian distribution
-% remove the log term here
-value = 1/4*lambda'/psi*lambda;
-end
